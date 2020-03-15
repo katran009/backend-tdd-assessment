a61481a9da83215ec471d2c5da10506dd4d8c632
Kathy Tran <44452243+katran009@users.noreply.github.com>
2020-03-15 17:18:06 -0500

    Add files via upload

--- echo.py
+++ echo.py
@@ -13,8 +13,8 @@ def create_parser():
     """Creates and returns an argparse cmd line option parser"""
     parser = argparse.ArgumentParser(description="Perform transformation on input text.")
     parser.add_argument("-u", "--upper", action="store_true", help="convert text to uppercase")
-    parser.add_argument("-l", "--lower", help="convert text to lowercase")
-    parser.add_argument("-t", "--title", help="convert text to titlecase")
+    parser.add_argument("-l", "--lower", action="store_true", help="convert text to lowercase")
+    parser.add_argument("-t", "--title", action="store_true", help="convert text to titlecase")
     parser.add_argument("text", help="text to be manipulated")
     return parser
 
